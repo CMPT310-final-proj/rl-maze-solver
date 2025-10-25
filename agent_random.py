e0f2ebf887792876c821651dcd434dde99732559
Pratham Vij <prathamvij10@gmail.com>
2025-10-25 11:57:01 -0700

    Delete agent_random.py

--- agent_random.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class RandomAgent:
-    def __init__(self, n_actions=4):
-        self.n_actions = n_actions
-
-    def choose_action(self):
-        return random.randint(0, self.n_actions - 1)
