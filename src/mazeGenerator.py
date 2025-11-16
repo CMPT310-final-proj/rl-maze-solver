c4b1222cb47d95e448a59c9add772dc150c669c2
Dylon <dylontd7@gmail.com>
2025-11-16 00:48:53 -0800

    added src folder and main. Also updated mazeGenerator.py

--- src/mazeGenerator.py
+++ src/mazeGenerator.py
@@ -4,18 +4,17 @@ import matplotlib.pyplot as plt
 import random as ran
 import numpy as np
 
-
-m = Maze()
-m.generator = Prims(ran.randint(15,30), ran.randint(20,40))
-m.generate()
-m.generate_entrances()
-m.grid[m.start] = 0
-m.grid[m.end] = 0
+def generateMaze():
+    m = Maze()
+    m.generator = Prims(ran.randint(9,11), ran.randint(9,11))
+    m.generate()
+    m.generate_entrances()
+    m.grid[m.start] = 0
+    m.grid[m.end] = 0
+    return np.array(m.grid), m.start, m.end
 
 def showMaze(grid):
     plt.figure(figsize=(10, 5))
     plt.imshow(grid, cmap=plt.cm.binary, interpolation='nearest')
     plt.xticks([]), plt.yticks([])
     plt.show()
-
-showMaze(m.grid)
